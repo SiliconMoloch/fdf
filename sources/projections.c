c50ea9bd106a032432c3a131b142a1aa7ee4591c
Yazid Bokhari <yazid.bokhari@hotmail.com>
2026-07-28 00:09:05 +0200

    Fixed mirrored/inversed point of view direcrly on the isometric_projection

--- sources/projections.c
+++ sources/projections.c
@@ -3,14 +3,14 @@
 
 t_vect2 isometric_projection(t_context context, uint64_t grid[2], uint64_t point_position)
 {
-    t_vect2     p;
-    float       points[2];
-    const float altitude = context.map.points[point_position].altitude * context.camera.zoom;
+    t_vect2 p;
+    const float x = (float)grid[X];
+    const float y = (float)grid[Y];
+    const float z = context.map.points[point_position].altitude * context.camera.zoom * 0.40f;
+    const float iso_x = (x - y) * cosf(ISO_ANGLE);
+    const float iso_y = (x + y) * sinf(ISO_ANGLE);
 
-    points[X] = (float)grid[X];
-    points[Y] = (float)grid[Y];
-
-    p.x = context.camera.zoom * (points[X] - points[Y]) * cos(ISO_ANGLE) + context.camera.position.x;
-    p.y = context.camera.zoom * (points[X] + points[Y]) * sin(ISO_ANGLE) + context.camera.position.y + altitude;
+    p.x =  iso_x * context.camera.zoom + context.camera.position.x;
+    p.y = -iso_y * context.camera.zoom + context.camera.position.y + z;
     return (p);
 }
