c697ca8eefc9f75efb02f4d57b6a0a06f77aed13
Yazid Bokhari <yabokhar@z1r6p4.42lyon.fr>
2026-08-11 19:29:19 +0200

    Removed ruins of alternative projections + starting to rework

--- sources/projections.c
+++ sources/projections.c
@@ -15,32 +15,26 @@ t_vect2 isometric_projection(t_context context, uint64_t grid[2], uint64_t point
     return (p);
 }
 
-#include <string.h>
-
 t_vect2 conic_projection(t_context context, uint64_t grid[2], uint64_t point_position)
 {
     t_vect2 p;
-    const float x = (float)grid[X];
-    const float y = (float)grid[Y];
-    const float z = context.map.points[point_position].altitude * context.camera.zoom;
-    const float iso_x = (x - y) * cosf(ISO_ANGLE);
-    const float iso_y = (x + y) * sinf(ISO_ANGLE);
 
-    p.x =  iso_x * context.camera.zoom + context.camera.position.x;
-    p.y = -iso_y * context.camera.zoom + context.camera.position.y + z;
+    (void)context;
+    (void)grid;
+    (void)point_position;
+    p.x = 0;
+    p.y = 0;
     return (p);
 }
 
 t_vect2 oblique_projection(t_context context, uint64_t grid[2], uint64_t point_position)
 {
     t_vect2 p;
-    const float x = (float)grid[X];
-    const float y = (float)grid[Y];
-    const float z = context.map.points[point_position].altitude * context.camera.zoom;
-    const float iso_x = (x - y) * cosf(ISO_ANGLE);
-    const float iso_y = (x + y) * sinf(ISO_ANGLE);
 
-    p.x =  iso_x * context.camera.zoom + context.camera.position.x;
-    p.y = -iso_y * context.camera.zoom + context.camera.position.y + z;
+    (void)context;
+    (void)grid;
+    (void)point_position;
+    p.x = 0;
+    p.y = 0;
     return (p);
 }
